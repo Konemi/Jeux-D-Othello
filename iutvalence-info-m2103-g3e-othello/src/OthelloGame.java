2e715bda39fd5e688d82ee251bc51dd351a973db
omel <jocelyn.omel@iut-valence.fr>
2015-05-20 16:39:43 +0200

    Creation of isMovepossible using a list of position and flippingDisks methods

--- iutvalence-info-m2103-g3e-othello/src/OthelloGame.java
+++ iutvalence-info-m2103-g3e-othello/src/OthelloGame.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * The progress of the game.
  * @author omelj
@@ -17,17 +20,28 @@ public class OthelloGame
 	 *  Player 2
 	 */
 	private Player player2;
-	private final int <Position> listPosition = {{1,-1},{1,0},{1,1},{0,1},{-1,1},{0,-1},{-1,-1},{-1,0}};
+	/**
+	 * la coordonnée de la liste des positions
+	 */
+	private int position;
+	
+	/**
+	 * List of all position around the square
+	 */
+	private final int[][] listPosition = {{this.position,-this.position},{this.position,0},{this.position,this.position},{0,this.position},{-this.position,this.position},{0,-this.position},{-this.position,-this.position},{-this.position,0}};
 	
 	
 	/**
 	 * initialization of the Game's board
+	 * @param player1 the player 1
+	 * @param player2 the player 2
 	 */
 	public OthelloGame(Player player1, Player player2)
 	{
 		this.board = new Board();
 		this.player1 = player1;
 		this.player2 = player2;
+		this.position = 1;
 	}
 	
 	/**
@@ -62,19 +76,57 @@ public class OthelloGame
 		return 0;
 	}
 	
-	/**
-	 * 
-	 * @param line line of the array
-	 * @param column line of the array
-	 *  @return true if the movement is possible
-	 */
-	private boolean isMovePossible(Position position)
+/**
+ * Determine if we can put a disk at the position
+ * @param position position of the tested square
+ * @param color color of the current player/ disk we want to put
+ * @return A list of every position until which we will have to switch disks or <tt>null<tt> if the square isn't available
+ */
+	private List<Position> isMovePossible(Position position, Disk color)
 	{
+		List<Position> possibleMovement = new ArrayList<Position>();
+		int i,j;
+		Position caseATester;
 		if (this.board.getDisk(this.board, position) != Disk.none)
+		{
+			return null;
+		}
+		for(int[] direction: this.listPosition)
+		{
+			i = position.obtenirNumeroDeLigne() + direction[0];
+			j = position.obtenirNumeroDeColonne() + direction[1];
+			caseATester = new Position(i,j);
+			if(this.board.getDisk(this.board, caseATester) == color || this.board.getDisk(this.board, caseATester) == Disk.none)
+			{
+				continue;
+			}
+			for(this.position=1;this.position<8;this.position++)
+			{
+				i = position.obtenirNumeroDeLigne() + direction[0];
+				j = position.obtenirNumeroDeColonne() + direction[1];
+				if (this.board.getDisk(this.board, caseATester) == color)
 				{
-					return false;
+                    possibleMovement.add(caseATester);
+					break;
 				}
-		return true;
+				if(this.board.getDisk(this.board, caseATester) == Disk.none)
+				{
+					break;
+				}
+			}
+		}
+		return possibleMovement;
 	}
 	
+	/**
+	 * flip every disks from the initial position to the final position
+	 */
+	public void flippingDisks(Player player, Position initialPosition, List<Position> finalPositions)
+	{
+		for(Position position: finalPositions)
+		{
+			//implementable with a hashmap instead of a list for finalPositions
+			player.putDisk(this.board, position);
+		}
+	}
 }
